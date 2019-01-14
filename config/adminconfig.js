1c6cc0befc6f299648e219c5b4edfdbec5232901
elishakay <kramer1346@gmail.com>
2019-01-14 14:42:04 +0200

    herokuapp

--- config/adminconfig.js
+++ config/adminconfig.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   //Port on which to deploy app.js
-  "port": 80,
+  "port": process.env.PORT || 80,
   //Your personal SendGrid API key. 
   "SENDGRID_API_KEY": process.env.SEND_GRID_KEY,
   //The id of the list you would like your client emails to (a number, can be found at the end the url path of your list)
